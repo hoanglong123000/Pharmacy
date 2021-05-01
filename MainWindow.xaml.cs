5053cd09dfb4a4e6a0f75eaae2467d7fc6bb45ff
Lee Ruan <49586994+hoanglong123000@users.noreply.github.com>
2021-05-01 21:39:42 +0700

    Adding Sourcecode

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QLThuoc
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    bool CheckOpenUC(string _Name)
+    {
+      bool result = false;
+      UserControl UC = Service.Current as UserControl;
+      if (UC.Name == string.Format("View{0}", _Name))
+      {
+        result = true;
+      }
+      return result;
+    }
+    void OpenViews(string _Name)
+    {
+      if (!CheckOpenUC(_Name))
+      {
+        Service.Navigate(_Name);
+      }
+    }
+
+    private void btnDoiTac_Click(object sender, EventArgs e)
+    {
+      OpenViews("DoiTac");
+    }
+
+    private void frame_Navigated(object sender, DevExpress.Xpf.WindowsUI.Navigation.NavigationEventArgs e)
+    {
+      if (e.Source.ToString() == "Home")
+      {
+        btnMenu.IsSelected = true;
+      }
+      else if (e.Source.ToString() == "Thuoc")
+      {
+        btnThuoc.IsSelected = true;
+      }
+      else if (e.Source.ToString() == "MuaHang")
+      {
+        btnMuaThuoc.IsSelected = true;
+      }
+      else if (e.Source.ToString() == "BanHang")
+      {
+        btnBanThuoc.IsSelected = true;
+      }
+      else if (e.Source.ToString() == "DoiTac")
+      {
+        btnDoiTac.IsSelected = true;
+      }
+    }
+
+    private void btnThuoc_Click(object sender, EventArgs e)
+    {
+      OpenViews("DMThuoc");
+    }
+
+
+    private void btnMuaThuoc_Click(object sender, EventArgs e)
+    {
+      OpenViews("MuaHang");
+    }
+
+    private void btnBanThuoc_Click(object sender, EventArgs e)
+    {
+      OpenViews("BanHang");
+    }
+
+    private void btnDoanhThu_Click(object sender, EventArgs e)
+    {
+      OpenViews("DoanhThu");
+    }
+
+    private void btnTonKho_Click(object sender, EventArgs e)
+    {
+      OpenViews("TonKho");
+    }
+  }
+}
---
+++ z.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QLThuoc
+{
+  public static class z
+  {
+
+    public static bool DeleteQuestion(string _Message = "Bạn có muốn xóa dữ liệu đang được chọn?", string _Caption = "Xác nhận xóa")
+    {
+      if (DXMessageBox.Show(_Message, _Caption, MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes) return true;
+      else return false;
+    }
+    public static string ConnectionString = @"Data Source=LAPTOP-5C698NK2\SQLEXPRESS;Initial Catalog=QLNhaThuoc;Integrated Security=True";
+        //public static string ConnectionString = @"Data Source=MACKENO\SQLEXPRESS;Initial Catalog=QLNhaThuoc;Integrated Security=True";
+
+        
+  }
+}
